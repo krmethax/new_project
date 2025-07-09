c36acfbcbf6000041ffc60aeff521c4a8e42f247
create-ole-github <methasit6061@gmail.com>
2025-07-09 18:09:08 +0700

    Docker

--- api/db.js
+++ api/db.js
@@ -1,12 +1,11 @@
-// db.js
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',          // ปรับ user ให้ตรงกับ MySQL ของคุณ
-  database: 'pet_sitter_app',
-  password: 'krmethax',
-  port: 3307             // ค่า port ของ MySQL โดยปกติคือ 3306
+  host: 'mysql-db',         
+  user: 'root',
+  password: 'krmethax',       
+  database: 'pet_sitter_app', 
+  port: 3307              
 });
 
 module.exports = {
